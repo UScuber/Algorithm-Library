f4087f557611cd7164f4b9af42b70611c037dc54
sota <sota1210nittc@gmail.com>
2021-11-10 22:35:27 +0900

    update include

--- graph/kruskal.cpp
+++ graph/kruskal.cpp
@@ -1,56 +1,8 @@
 #include <bits/stdc++.h>
+#include "template.hpp"
+#include "unionfind.hpp"
 using namespace std;
 using ll = long long;
-
-/* template */
-template <class T>
-struct edge {
-  int from,to;
-  T cost;
-  int idx;
-  edge(){};
-  edge(int f, int t, T c=1, int i=-1) : from(f), to(t), cost(c), idx(i){}
-  edge(int t) : to(t), from(-1), cost(1), idx(-1){}
-  operator int() const{ return to; }
-  bool operator<(const edge &e){ return cost < e.cost; }
-};
-template <class T>
-struct Graph : vector<vector<edge<T>>> {
-  Graph(){}
-  Graph(const int &n) : vector<vector<edge<T>>>(n){}
-  void add_edge(int a, int b, T c=1, int i=-1){
-    (*this)[a].push_back({ a, b, c, i });
-  }
-};
-using graph = Graph<int>;
-/* template */
-
-/* unionfind */
-struct UnionFind {
-  vector<int> d;
-  UnionFind(int n) : d(n, -1){}
-  int root(int x){
-    if(d[x] < 0) return x;
-    return d[x] = root(d[x]);
-  }
-  bool unite(int x, int y){
-    x = root(x);
-    y = root(y);
-    if(x == y) return false;
-    if(d[x] > d[y]) swap(x,y);
-    d[x] += d[y];
-    d[y] = x;
-    return true;
-  }
-  bool same(int x, int y){
-    return root(x) == root(y);
-  }
-  int size(int x){
-    return -d[root(x)];
-  }
-};
-/* unionfind */
-
 
 template <class T>
 ll kruskal(vector<edge<T>> &es, int V){
