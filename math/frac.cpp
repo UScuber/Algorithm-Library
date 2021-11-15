d709a73a66b3478b9cb380d3bbcf7d9bb9b5239a
sota <sota1210nittc@gmail.com>
2021-11-15 23:59:20 +0900

    update frac.cpp

--- math/frac.cpp
+++ math/frac.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+#include <cassert>
 using ll = long long;
 using namespace std;
 
 struct frac {
   ll a,b;
-  frac(ll a = 0, ll b = 1) : a(a), b(b){}
+  frac(const ll a = 0, const ll b = 1) : a(a), b(b){}
   frac &operator*=(const ll &d){
     a *= d;
     return *this;
@@ -15,6 +16,7 @@ struct frac {
     return *this;
   }
   frac &operator/=(const frac &d){
+    assert(d); //0 division
     a *= d.b; b *= d.a;
     reduce();
     return *this;
@@ -43,27 +45,30 @@ struct frac {
   bool operator==(const frac &d) const{ return a == d.a && b == d.b;}
   bool operator<(const frac &d) const{ return (double)a/b < (double)d.a/d.b; }
   bool operator>(const frac &d) const{ return (double)a/b > (double)d.a/d.b; }
+  operator bool() const{ return !a; }
   void reduce(){
     ll g = gcd(abs(a), abs(b));
     if(a < 0 && b < 0) g = -g;
     a /= g; b /= g;
   }
-  ll gcd(ll a, ll b){
-    if(a % b == 0) return b;
+  ll gcd(const ll &a, const ll &b){
+    if(!b) return a;
     return gcd(b, a % b);
   }
   void debug(){ cerr << a << "/" << b << " "; }
   friend istream &operator>>(istream &is, frac &d){ return is >> d.a; }
   friend ostream &operator<<(ostream &os, const frac &d){
-    printf("%.10lf", (double)d.a / d.b);
-    //if(!d.a) os << 0;
-    //else if(d.b < 0){
-    //  if(d.b == -1) os << d.a;
-    //  else os << d.a << "/" << d.b;
-    //}else{
-    //  if(d.b == 1) os << d.a;
-    //  else os << d.a << "/" << d.b;
-    //}
+    printf("%lf", (double)d.a / d.b);
+    /*
+    if(!d.a) os << 0;
+    else if(d.b < 0){
+      if(d.b == -1) os << d.a;
+      else os << d.a << "/" << d.b;
+    }else{
+      if(d.b == 1) os << d.a;
+      else os << d.a << "/" << d.b;
+    }
+    */
     return os;
   }
 };
