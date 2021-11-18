3377226d90661cf4a40e59f7fa64579cbdf7823f
sota <sota1210nittc@gmail.com>
2021-11-18 17:57:31 +0900

    add euler

--- math/math.cpp
+++ math/math.cpp
@@ -14,6 +14,7 @@ T lcm(T a, T b){
   return a / gcd(a, b) * b;
 }
 
+
 ll modPow(ll a, ll n, ll p){
   if(!n) return 1;
   if(n == 1) return a % p;
@@ -21,6 +22,7 @@ ll modPow(ll a, ll n, ll p){
   ll t = modPow(a, n / 2, p);
   return (t * t) % p;
 }
+
 
 ll ext_gcd(ll a, ll b, ll &x, ll &y){
   ll d = a;
@@ -31,6 +33,7 @@ ll ext_gcd(ll a, ll b, ll &x, ll &y){
   return d;
 }
 
+
 pair<ll, ll> crt(ll b1, ll m1, ll b2, ll m2){
   ll p, q;
   ll d = ext_gcd(m1, m2, p, q); // p is inv of m1/d (mod. m2/d)
@@ -39,4 +42,16 @@ pair<ll, ll> crt(ll b1, ll m1, ll b2, ll m2){
   ll tmp = (b2 - b1) / d * p % (m2/d);
   ll r = ((b1 + m1 * tmp) % m + m) % m;
   return make_pair(r, m);
+}
+
+
+ll euler(ll n){
+  ll res = n, k = n;
+  for(ll i = 2; i*i <= k; i++){
+    if(n % i) continue;
+    res = res / i * (i - 1);
+    while(n % i == 0) n /= i;
+  }
+  if(n != 1) res = res / n * (n - 1);
+  return res;
 }
