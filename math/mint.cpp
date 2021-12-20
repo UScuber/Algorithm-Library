ea2708de6066e35a679521380c90339c15215b6f
sota <sota1210nittc@gmail.com>
2021-12-20 19:41:41 +0900

    update mint

--- math/mint.cpp
+++ math/mint.cpp
@@ -42,10 +42,13 @@ struct mint{
   mint operator*(const mint &a) const{ return mint(*this) *= a; }
   mint pow(ll t) const{
     if(!t) return 1;
-    mint a = pow(t >> 1);
-    a *= a;
-    if (t & 1) a *= *this;
-    return a;
+    mint res = 1, v = *this;
+    while(t){
+      if(t & 1) res *= v;
+      v *= v;
+      t >>= 1;
+    }
+    return res;
   }
   mint inv() const{ return pow(mod - 2); }
   mint &operator/=(const mint &a){ return (*this) *= a.inv(); }
