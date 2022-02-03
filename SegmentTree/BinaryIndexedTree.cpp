745582a8aae73fdac09fd41fa9abccb8c5fa2016
sota <sota1210nittc@gmail.com>
2022-02-03 09:04:08 +0900

    udpate BinaryIndexedTree

--- SegmentTree/BinaryIndexedTree.cpp
+++ SegmentTree/BinaryIndexedTree.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 template <class T>
 struct Bit {
-  Bit(int _n) : n(_n), d(n){}
+  Bit(int _n = 0) : n(_n), d(n){}
+  Bit(const vector<T> &v) : d(v), n(v.size()){
+    for(int i = 1; i <= n; i++){
+      const int j = i + (i & -i);
+      if(j <= n) d[j - 1] += d[i - 1];
+    }
+  }
   T sum(int l, int r) const{
     assert(0 <= l && l <= r && r <= n);
     return sum(r) - sum(l);
