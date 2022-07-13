9e337c217dbb775f60c6d74951945d3af2c6bdbc
sota <sota1210nittc@gmail.com>
2022-07-13 09:36:21 +0900

    update random

--- others/random.hpp
+++ others/random.hpp
@@ -1,13 +1,21 @@
-#include <bits/stdc++.h>
-#include <random>
-using namespace std;
+using ll = long long;
 
-random_device seed_gen;
-mt19937_64 engine(seed_gen());
+inline unsigned int randxor32() noexcept{
+  static unsigned int y = 2463534242;
+  y = y ^ (y << 13); y = y ^ (y >> 17);
+  return y = y ^ (y << 5);
+}
+// returns random [l, r)
+inline int rnd(const int l, const int r) noexcept{
+  return randxor32() % (r - l) + l;
+}
 
-template<class T>
-T rnd(T min_val, T max_val){
-  assert(min_val <= max_val);
-  uniform_int_distribution <T> dist(min_val, max_val);
-  return dist(engine);
+unsigned long long randxor64(){
+  static unsigned long long x = 88172645463325252ULL;
+  x = x ^ (x << 7);
+  return x = x ^ (x >> 9);
+}
+// returns random [l, r)
+inline long long rnd(const ll l, const ll r) noexcept{
+  return randxor64() % (r - l) + l;
 }
