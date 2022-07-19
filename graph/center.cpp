e8736f5eaae7f03ad0accb0c5744085593fc2033
sota <sota1210nittc@gmail.com>
2022-07-19 13:01:55 +0900

    update center

--- graph/center.cpp
+++ graph/center.cpp
@@ -2,11 +2,13 @@
 #include "template.hpp"
 using namespace std;
 
-vector<int> find_centor(const graph &g){
-  int n = g.size();
+#include <functional>
+template <class graph>
+pair<vector<int>, int> find_centor(const graph &g){
+  const int n = g.size();
   vector<int> dist(n), prev(n, -1);
   function<void(int,int)> dfs = [&](int pos, int par){
-    for(auto &x : g[pos]) if(x != par){
+    for(const auto &x : g[pos]) if(x != par){
       dist[x] = dist[pos] + 1;
       prev[x] = pos;
       dfs(x, pos);
@@ -17,12 +19,12 @@ vector<int> find_centor(const graph &g){
   dist.assign(n, 0); prev.assign(n, -1);
   dfs(pos, -1);
   pos = max_element(dist.begin(), dist.end()) - dist.begin();
-  int max_dist = dist[pos];
-  int longest = max_dist;
+  const int longest = dist[pos];
+  int max_dist = longest;
   vector<int> res;
   for(int i = pos; i != -1; i = prev[i]){
     if(max_dist == longest/2 || max_dist == (longest+1)/2) res.push_back(i);
     max_dist--;
   }
-  return res;
+  return { res, longest };
 }
