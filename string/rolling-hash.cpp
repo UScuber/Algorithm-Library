226902369fe55a9750720c16ea79f0f825bfb017
sota <sota1210nittc@gmail.com>
2023-01-09 23:36:14 +0900

    add strcmp (rolling-hash)

--- string/rolling-hash.cpp
+++ string/rolling-hash.cpp
@@ -5,18 +5,19 @@ using ll = long long;
 #include <chrono>
 #include <random>
 using ull = unsigned long long;
-template <int max_len>
+template <class T, int max_len>
 struct Hash {
   static constexpr ull m = (1ULL << 61) - 1;
   const ull base;
   vector<ull> h;
-  Hash(const ull base, const ull power[]) : base(base), power(power){}
+  const T data;
+  Hash(const T &d, const ull base, const ull power[]) : data(d), base(base), power(power){}
   inline ull query(int l, int r) const{
     assert(max_len >= r - l);
-    assert(0 <= l && l <= r && r < h.size());
+    assert(0 <= l && l <= r && r <= h.size());
     return add(h[r], m - mul(h[l], power[r - l]));
   }
-  void combine(const Hash<max_len> &a){
+  void combine(const Hash<T, max_len> &a){
     const int len = h.size();
     h.insert(h.end(), a.h.begin()+1, a.h.end());
     const int tot_len = h.size();
@@ -26,18 +27,18 @@ struct Hash {
       h[i] = add(val, h[i]);
     }
   }
-  inline int size() const{ return h.size(); }
+  inline int size() const noexcept{ return (int)h.size()-1; }
   private:
   const ull *power;
-  inline ull add(ull a, const ull b) const{
+  inline ull add(ull a, const ull b) const noexcept{
     if((a += b) >= m) a -= m;
     return a;
   }
-  inline ull mul(const ull a, const ull b) const{
+  inline ull mul(const ull a, const ull b) const noexcept{
     const __uint128_t c = (__uint128_t)a * b;
     return add(c >> 61, c & m);
   }
-  inline ull fma(const ull a, const ull b, const ull c) const{
+  inline ull fma(const ull a, const ull b, const ull c) const noexcept{
     const __uint128_t d = (__uint128_t)a * b + c;
     return add(d >> 61, d & m);
   }
@@ -52,9 +53,9 @@ struct RollingHash {
       power[i + 1] = mul(power[i], base);
     }
   }
-  Hash<max_len> gen(const string &s) const{
+  Hash<string, max_len> gen(const string &s) const{
     const int len = s.size();
-    Hash<max_len> hash(base, power);
+    Hash<string, max_len> hash(s, base, power);
     hash.h.resize(len + 1);
     for(int i = 0; i < len; i++){
       hash.h[i+1] = fma(hash.h[i], base, s[i]);
@@ -62,27 +63,31 @@ struct RollingHash {
     return hash;
   }
   template <class T>
-  Hash<max_len> gen(const vector<T> &s) const{
+  Hash<vector<T>, max_len> gen(const vector<T> &s) const{
     const int len = s.size();
-    Hash<max_len> hash(base, power);
+    Hash<vector<T>, max_len> hash(s, base, power);
     hash.h.resize(len + 1);
     for(int i = 0; i < len; i++){
       hash.h[i+1] = fma(hash.h[i], base, s[i]);
     }
     return hash;
   }
-  ull combine(ull h1, ull h2, ull h2_len) const{
+  ull combine(const ull h1, const ull h2, const ull h2_len) const{
     assert(max_len >= h2_len);
     return fma(h1, power[h2_len], h2);
   }
-  ull combine(const Hash<max_len> &a, int l1, int r1, const Hash<max_len> &b, int l2, int r2) const{
+  template <class T>
+  ull combine(const Hash<T,max_len> &a, const int l1, const int r1,
+              const Hash<T,max_len> &b, const int l2, const int r2) const{
     assert(max_len >= r2 - l2);
     return fma(a.query(l1, r1), power[r2-l2], b.query(l2, r2));
   }
-  int lcp(const Hash<max_len> &a, int l1, int r1, const Hash<max_len> &b, int l2, int r2) const{
-    assert(0 <= l1 && l1 <= r1 && r1 < a.size());
-    assert(0 <= l2 && l2 <= r2 && r2 < b.size());
-    const int len = min(r1 - l1, r2 - l2);
+  template <class T>
+  int lcp(const Hash<T,max_len> &a, const int l1, const int r1,
+          const Hash<T,max_len> &b, const int l2, const int r2) const{
+    assert(0 <= l1 && l1 <= r1 && r1 <= a.size());
+    assert(0 <= l2 && l2 <= r2 && r2 <= b.size());
+    const int len = min(r1-l1, r2-l2);
     int left = 0, right = len + 1;
     while(left + 1 < right){
       const int mid = (left + right) / 2;
@@ -91,6 +96,16 @@ struct RollingHash {
     }
     return left;
   }
+  template <class T>
+  int strcmp(const Hash<T,max_len> &a, const int l1, const int r1
+            ,const Hash<T,max_len> &b, const int l2, const int r2) const{
+    assert(0 <= l1 && l1 <= r1 && r1 <= a.size());
+    assert(0 <= l2 && l2 <= r2 && r2 <= b.size());
+    const int pos = lcp(a, l1, r1, b, l2, r2);
+    if(pos < min(r1-l1, r2-l2)) return a.data[l1+pos] < b.data[l2+pos] ? 1 : -1;
+    if(r1-l1 == r2-l2) return 0;
+    return r1-l1 < r2-l2 ? 1 : -1;
+  }
   private:
   ull power[max_len + 1];
   ull rnd() const{
@@ -98,15 +113,15 @@ struct RollingHash {
     uniform_int_distribution<ull> rand(2, m-1);
     return rand(mt);
   }
-  inline ull add(ull a, const ull b) const{
+  inline ull add(ull a, const ull b) const noexcept{
     if((a += b) >= m) a -= m;
     return a;
   }
-  inline ull mul(const ull a, const ull b) const{
+  inline ull mul(const ull a, const ull b) const noexcept{
     const __uint128_t c = (__uint128_t)a * b;
     return add(c >> 61, c & m);
   }
-  inline ull fma(const ull a, const ull b, const ull c) const{
+  inline ull fma(const ull a, const ull b, const ull c) const noexcept{
     const __uint128_t d = (__uint128_t)a * b + c;
     return add(d >> 61, d & m);
   }
