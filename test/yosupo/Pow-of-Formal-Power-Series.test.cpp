a0f3707720b04f8229c504b393935ebf6143c1c6
sota <sota1210nittc@gmail.com>
2023-03-14 00:33:27 +0900

    verify convolution,fps

---
+++ test/yosupo/Pow-of-Formal-Power-Series.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/pow_of_formal_power_series"
+
+#include "../../template/template.hpp"
+
+#include "../../math/fps/fps.hpp"
+#include "../../math/mint.hpp"
+
+using mint = Mint<998244353>;
+using fps = FPS<mint>;
+
+int main(){
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  int n; ll m;
+  cin >> n >> m;
+  fps a(n);
+  rep(i, n) cin >> a[i];
+  const fps b = a.pow(m);
+  rep(i, n) cout << b[i] << " \n"[i == n - 1];
+}
