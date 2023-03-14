a628fcff714232e26070eeb06348484b111570a8
sota <sota1210nittc@gmail.com>
2023-03-14 18:00:32 +0900

    verify maxflow,convolution,fastio,kitamasa

---
+++ test/aoj/GRL/GRL_6_A.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/GRL_6_A"
+
+#include "../../../template/template.hpp"
+
+#include "../../../graph/fordfulkerson.hpp"
+
+int main(){
+  int n,m;
+  cin >> n >> m;
+  maxflow<int> g(n);
+  rep(i, m){
+    int a,b,c;
+    cin >> a >> b >> c;
+    g.add_edge(a, b, c);
+  }
+  cout << g.max_flow(0, n-1) << "\n";
+}
