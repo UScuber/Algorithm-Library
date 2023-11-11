3d7822eb86ac42c8e52cd03f7d8606162428eaba
sota <sota1210nittc@gmail.com>
2023-11-12 00:30:44 +0900

    update fastio (add load,flush)

--- others/fastIO.hpp
+++ others/fastIO.hpp
@@ -14,20 +14,33 @@ struct PreCalc {
 static constexpr PreCalc pr;
 struct FastIO {
   template <class T>
-  using enable_if_integer = enable_if_t<is_integral<T>::value || is_same<T, __int128_t>::value || is_same<T, __uint128_t>::value>;
-  static constexpr int buf_size = 1 << 26;
+  using enable_if_integer = enable_if_t<is_integral<T>::value || is_same<T, __int128_t> || is_same<T, __uint128_t>::value>;
+  static constexpr int buf_size = 1 << 20;
+  static constexpr int rem = 1 << 6;
   char in_buf[buf_size], *in_cur = in_buf;
   char out_buf[buf_size], *out_cur = out_buf;
-  #define gc() (*in_cur++)
   FastIO(){
     fread(in_buf, 1, buf_size, stdin);
   }
   ~FastIO(){
     fwrite(out_buf, 1, out_cur - out_buf, stdout);
   }
-  void through(){
-    while(*in_cur <= ' ') in_cur++;
+  void load(){
+    const int len = in_buf + buf_size - in_cur;
+    memmove(in_buf, in_cur, len);
+    in_cur = in_buf;
+    fread(in_buf + len, 1, buf_size - len, stdin);
   }
+  void flush(){
+    fwrite(out_buf, 1, out_cur - out_buf, stdout);
+    out_cur = out_buf;
+  }
+  void through(){
+    if(in_cur - in_buf >= buf_size - rem) load();
+    while(*in_cur <= ' ') in_cur++;
+    assert(in_buf <= in_cur && in_cur < in_buf + buf_size);
+  }
+  #define gc() (*in_cur++)
   template <class T, enable_if_integer<T>* = nullptr>
   inline void read(T &x){
     through();
@@ -40,10 +53,15 @@ struct FastIO {
   }
   inline void read(string &x){
     through();
-    char *p = in_cur;
-    while(*p > ' ') p++;
-    x = string(in_cur, p);
-    in_cur = p;
+    x.clear();
+    while(true){
+      char *p = in_cur;
+      while(*p > ' ' && p - in_buf < buf_size - rem) p++;
+      copy(in_cur, p, back_inserter(x));
+      in_cur = p;
+      if(*p <= ' ') break;
+      load();
+    }
   }
   inline void read(char &x){
     through();
@@ -55,6 +73,7 @@ struct FastIO {
   inline void out(T x){
     static constexpr int tmp_size = sizeof(T)*5/2;
     static char tmp[tmp_size];
+    if(out_cur - out_buf >= buf_size - rem) flush();
     if(!x){ pc('0'); return; }
     if(x < 0){ pc('-'); x = -x; }
     int idx = tmp_size;
@@ -83,10 +102,13 @@ struct FastIO {
     out_cur += tmp_size - idx;
   }
   inline void out(const string &s){
-    copy(s.begin(), s.end(), out_cur);
-    out_cur += s.size();
+    flush();
+    fwrite(s.c_str(), 1, s.size(), stdout);
   }
-  inline void out(const char c){ pc(c); }
+  inline void out(const char c){
+    if(out_cur - out_buf >= buf_size - rem) flush();
+    pc(c);
+  }
   #undef pc
   template <class T>
   friend FastIO &operator>>(FastIO &io, T &x){
